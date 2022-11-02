5678519e0ca6f7c51dc8ccbe93442acbaa2cc917
Alexander Borisenko <79612696784@yandex.ru>
2022-11-02 21:55:11 +0300

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,14 @@ public class Main {
     public static void main(String[] args) {
         BonusService service = new BonusService();
 
+        long expected = 30;
         long amount = 1000_60;
         boolean registered = true;
 
-        service.calculate(amount, registered);
+        long actual = service.calculate(amount, registered);
+
+        boolean result = actual == expected;
+        System.out.println(result);
     }
 }
 
